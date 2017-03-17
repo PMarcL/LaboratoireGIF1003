e8c498f02ef3d22ef05ebd88556f96438ee87ec3
Étudiant <pmleva@hotmail.com>
2017-03-17 13:38:27 -0400

    Labo 8 sphere

---
+++ Laboratoire8/SphereLib/Sphere.cpp
@@ -0,0 +1,56 @@
+/**
+ * \file Sphere.cpp
+ * \brief 
+ * \author Pierre-Marc Levasseur
+ * \date 2017-03-17
+ */
+
+#include "ContratException.h"
+#include "Sphere.h"
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+namespace labo8 {
+
+Sphere::Sphere() :
+		m_rayon(0) {
+	POSTCONDITION(m_rayon == 0);
+	INVARIANTS();
+}
+
+Sphere::Sphere(double rayon) :
+		m_rayon(rayon) {
+	PRECONDITION(rayon >= 0);
+	POSTCONDITION(m_rayon == rayon);
+	INVARIANTS();
+}
+
+Sphere::~Sphere() {
+}
+
+double Sphere::reqRayon() const {
+	return m_rayon;
+}
+
+void Sphere::asgRayon(double rayon) {
+	PRECONDITION(rayon >= 0);
+	m_rayon = rayon;
+}
+
+void Sphere::verifieInvariant() const {
+	INVARIANT(m_rayon >= 0);
+}
+
+double Sphere::reqVolume() const {
+	return 4.0 / 3.0 * M_PI * pow(m_rayon, 3);
+}
+
+std::string Sphere::reqSphereFormate() const {
+	ostringstream os;
+	os << "Sphere de rayon " << reqRayon() << " et de volume " << reqVolume();
+	return os.str();
+}
+} /* namespace labo8 */
+
---
+++ Laboratoire8/SpherePrincipal/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * \file main.cpp
+ * \brief 
+ * \author Pierre-Marc Levasseur
+ * \date 2017-03-17
+ */
+
+#include "Sphere.h"
+
+int main() {
+	labo8::Sphere test;
+	return 0;
+}
+
